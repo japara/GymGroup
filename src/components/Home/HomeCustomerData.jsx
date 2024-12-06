0c22ffa372d2d6da7e063adfad79f59e9560b979
japara <72559893+japara@users.noreply.github.com>
2024-12-06 20:31:02 +0400

    customerdata responsive

--- src/components/Home/HomeCustomerData.jsx
+++ src/components/Home/HomeCustomerData.jsx
@@ -2,21 +2,29 @@ import React from "react";
 
 export default function HomeCustomerData() {
   return (
-    <div className="w-full bg-[#D7FD44] h-[100px] flex justify-between items-center px-[80px]">
-      <img src="Star 1.png" className="w-fit h-fit"></img>
-      <div>
-        <p className="font-[800] text-[32px]">200 +</p>
-        <p className="font-[500] text-[14px]">Happy Costumers</p>
+    <div className="w-full bg-[#D7FD44] h-[100px] flex flex-row justify-evenly items-center">
+      <img
+        src="Star 1.png"
+        className="w-fit h-fit hidden md:block"
+        alt="Star"
+      />
+      <div className="text-center flex-wrap md:text-left md:block">
+        <p className="font-[700] text-[18px] md:text-[32px]">200 +</p>
+        <p className="font-[700] text-[18px]">Happy Customers</p>
       </div>
-      <div>
-        <p className="font-[800] text-[32px]">3 +</p>
-        <p className="font-[500] text-[14px]">Years Of Training Experience</p>
+      <div className="text-center flex-wrap md:text-left md:block">
+        <p className="font-[800] text-[18px] md:text-[32px]">3 +</p>
+        <p className="font-[700] text-[18px]">Years Of Training Experience</p>
       </div>
-      <div>
-        <p className="font-[800] text-[32px]">98 %</p>
-        <p className="font-[500] text-[14px]">Customer Satisfaction</p>
+      <div className="text-center flex-wrap md:text-left md:block">
+        <p className="font-[800] text-[18px] md:text-[32px]">98 %</p>
+        <p className="font-[700] text-[18px]">Customer Satisfaction</p>
       </div>
-      <img src="/Star 1.png" className="w-fit h-fit"></img>
+      <img
+        src="/Star 1.png"
+        className="w-fit h-fit hidden md:block"
+        alt="Star"
+      />
     </div>
   );
 }
